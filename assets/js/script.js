1864129a1e2c641963329f69b98222ade39c8f48
mika <tiffinante@gmail.com>
2024-05-21 23:50:36 +0200

    -

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,32 @@
 // Background
-// Array with the paths to the background images
 const backgrounds = [
     "./assets/img/backgrounds/marek-piwnicki-wgsu3WzFZ5c-unsplash.jpg",
     "./assets/img/backgrounds/marek-piwnicki-zbowrjiVx2c-unsplash.jpg",
     "./assets/img/backgrounds/marek-piwnicki-ZuI0QS361bg-unsplash.jpg"
 ];
 
-// Function that selects a random image from the array
 function getRandomBackground() {
     const randomIndex = Math.floor(Math.random() * backgrounds.length);
     return backgrounds[randomIndex];
 }
 
-// Function that sets the selected background image
 function setBackground(event) {
-    event.preventDefault(); // Prevents the page view from changing (without this line the page scrolls up)
+    event.preventDefault();
     const select = document.getElementById('backgroundSelect');
     const selectedBackground = select.value;
     const backgroundDiv = document.getElementById('background');
     backgroundDiv.style.backgroundImage = `url('${selectedBackground}')`;
 }
 
-// Event listener for the button
 document.getElementById('setBackgroundButton').addEventListener('click', setBackground);
 
-// Set random background image when loading website
 window.onload = function() {
     const select = document.getElementById('backgroundSelect');
     const backgroundDiv = document.getElementById('background');
-    // Check that we are on the home.html page
     if (window.location.pathname.endsWith('privacy.html')) {
-        const homeBackground = backgrounds[2]; // Picture 3
+        const homeBackground = backgrounds[2];
         backgroundDiv.style.backgroundImage = `url('${homeBackground}')`;
         select.value = homeBackground;
-    /*
-    } else if (window.location.pathname.endsWith('index.html') || window.location.pathname.endsWith('')) {
-        const homeBackground = backgrounds[1]; // Picture 2
-        backgroundDiv.style.backgroundImage = `url('${homeBackground}')`;
-        select.value = homeBackground;
-    */
     } else {
         const randomBackground = getRandomBackground();
         backgroundDiv.style.backgroundImage = `url('${randomBackground}')`;
@@ -46,7 +34,6 @@ window.onload = function() {
     }
 }
 // !Background
-
 
 
 // Scroll Button
@@ -81,4 +68,6 @@ document.addEventListener("DOMContentLoaded", function() {
 // !Scroll Button
 
 
+// Scroll-triggered Animation
 
+// !Scroll-triggered Animation
